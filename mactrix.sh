1f4095ca7722f28914afac5a2406eba705130aa0
root <fillipedem@gmail.com>
2018-10-27 21:44:34 -0300

    Pequeno Script para inicialização da aplicação

---
+++ mactrix.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+#####################################################
+#                                                   #
+#    Monitoramento de Redes WiFi - MACTRIX          #
+#                                                   #
+# Uso:                                              #
+# mactrix [Interface Wireless] [Channel]            #
+#                                                   #                               
+# -A interface wireless deve está em modo monitor   #
+#                                                   #  
+# -Se nenhum canal for especifico, os canais 1/6/11 #
+#  seram utilizados                                 # 
+#####################################################
+
+#########################
+# Checando dependencias #
+#########################
+
+
+########################################
+# Checando se os parametros do programa#
+# foram passados corretamente          #
+########################################
+if [ -z $1 ]; then
+  echo "ERRO: Interface Wireless não informada"
+  exit
+fi
+IWIRELESS=$1
+
+if [ -z $2 ]; then
+  CHANNEL="1 6 11"
+else
+  CHANNEL=$2
+fi
+echo "Canais escolhidos: $CHANNEL - OK"
+################################################
+# Essa parte do codigo checa se a placa Wifi   #
+# está presente e se está em modo monitor      #
+################################################
+function check_iw_name {
+  # checa se a interface wireless existe
+  local TMP=$( iwconfig 2> /dev/null | grep -i -o $1)
+  if [ -z $TMP ]; then
+    echo "ERRO: Interface $1 não existe!"
+    exit
+  else
+    echo "Interface $1 - OK"
+  fi
+}
+
+function check_iw_mode {
+  # checa se a interface está em modo monitor
+  local TMP=$( iwconfig $1 2> /dev/null | grep -i -o monitor )
+  if [ -z $TMP ]; then
+    echo "ERRO: A interface $1 não está em modo monitor"
+    exit
+  else
+   echo "Interface $1 em modo monitor! - OK"
+  fi 
+}
+
+check_iw_name $IWIRELESS
+check_iw_mode $IWIRELESS
+#######################
+# Alterando os canais #                      
+#######################
+iwconfig $IWIRELESS channel 1
+iwconfig $IWIRELESS channel 6
+iwconfig $IWIRELESS channel 11
+#######################
+# Iniciando o MACTRIX #
+#######################
+sudo python3 flaskapp.py $IWIRELESS
+
+
+
+
+
+          
